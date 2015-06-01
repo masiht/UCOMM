ac6ecbeb9608290ef5d646f813ed68aac5c75e28
Masih Tabrizi <masih_tabrizi@softwaremerchant.com>
2015-06-01 12:45:04 -0400

    Job model improved.

--- src/main/java/com/att/ucomm/models/Job.java
+++ src/main/java/com/att/ucomm/models/Job.java
@@ -1,21 +1,31 @@
-package com.att.ucomm;
+package com.att.ucomm.models;
+
+import java.util.*;
+
 
 public class Job {
 
 	private String techId;
 	private int ban;
 	private int jobId;
+	private Date day;
+	private Date startDate;
+	private Date endDate;
 	private String status;
 	private String jobType;
+	
 
 	public Job() {
 
 	}
 
-	public Job(String techid, int ban, int jobid, String status, String jobtype) {
+	public Job(String techid, int ban, int jobid, Date day, Date sDate, Date eDate, String status, String jobtype) {
 		this.techId = techid;
 		this.ban = ban;
 		this.jobId = jobid;
+		this.day = day;
+		this.startDate = sDate;
+		this.endDate = eDate;
 		this.status = status;
 		this.jobType = jobtype;
 	}
@@ -30,6 +40,30 @@ public class Job {
 	
 	public void setBan(int ban) {
 		this.ban = ban;
+	}
+	
+	public void setDay(Date day) {
+		this.day = day;
+	}
+	
+	public Date getDay() {
+		return this.day;
+	}
+	
+	public void setStartDate(Date sDate) {
+		this.startDate = sDate;
+	}
+	
+	public Date getStartDate() {
+		return this.startDate;
+	}
+	
+	public void setEndDate(Date eDate) {
+		this.endDate = eDate;
+	}
+	
+	public Date getEndDate() {
+		return this.endDate;
 	}
 
 	public int getBan() {
@@ -65,8 +99,12 @@ public class Job {
 	public String toString() {
 		return new StringBuffer(" Tech ID : ").append(this.techId)
 				.append(" BAN : ").append(this.ban)
-				.append(" Job ID : ").append(this.jobId).append(" Status : ")
-				.append(this.status).append(" Job Type : ").append(this.jobType).toString();
+				.append(" Job ID : ").append(this.jobId)
+				.append(" Date: ").append(this.day)
+				.append(" Start: ").append(this.startDate)
+				.append(" End: ").append(this.endDate)
+				.append(" Status : ").append(this.status)
+				.append(" Job Type : ").append(this.jobType).toString();
 	}
 
 }
