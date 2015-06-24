d6dc099270eefd12b273a29c143b3aa905ce1003
Masih Tabrizi <masih_tabrizi@softwaremerchant.com>
2015-06-24 16:13:53 -0400

    JDBC for location service.

--- src/main/java/com/att/ucomm/TechLocationService.java
+++ src/main/java/com/att/ucomm/TechLocationService.java
@@ -13,12 +13,11 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import com.att.ucomm.controllers.DatabaseController;
 import com.att.ucomm.models.Tech;
 
 @Path("/techlocation")
 public class TechLocationService {
-
-	List<Tech> allTech = new ArrayList<Tech>();
 
 	@GET
 	@Path("/{techid}")
@@ -26,11 +25,11 @@ public class TechLocationService {
 	public HashMap<String, List> techLocations(@PathParam("techid") String techid) {
 
 		HashMap<String, List> techlocations = new HashMap<String, List>();
-		loadData();
+		DatabaseController db = new DatabaseController();
 		String[] idList = techid.split("-");
 
 
-		for (Tech t : allTech) {
+		for (Tech t : db.loadTechs()) {
 			for (String id : idList) {
 				if (t.getId().equals(id)) {
 					List<String> location = Arrays.asList(t.getLatitude(), t.getLongitude());
@@ -39,16 +38,5 @@ public class TechLocationService {
 			}
 		}
 		return techlocations;
-	}
-
-
-	public void loadData() {
-
-		Tech t1 = new Tech("Mark Hughes", "tm1038", "40.397549", "-74.135592", 0, "jj0000");
-		Tech t2 = new Tech("Marry Molehill", "mk1017", "40.384170", "-74.161266", 1, "jj0000");
-		Tech t3 = new Tech("Richard Nelson", "ll1010", "40.364170", "-74.191266", 2, "jj0000");
-		allTech.add(t1);
-		allTech.add(t2);
-		allTech.add(t3);
 	}
 }
